e002ca6a0fba746a7eddd5dc62fc9f4d852ac801
ValeriiaBond <valeriaredkina19@gmail.com>
2021-02-24 15:33:05 +0200

    initial commit

---
+++ js/js.js
@@ -0,0 +1,36 @@
+
+
+
+$(document).ready(function() {
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.menu__body').toggleClass('open-menu');
+    });
+
+    $('.benefits__carousel').owlCarousel({
+    	loop:true,
+    	margin:10,
+    	nav:true,
+    	autoplay:true,
+    	responsive:{
+        0:{
+            items:1
+        }
+    	}
+	});
+
+		$('.slider__body').owlCarousel({
+    	loop:true,
+    	margin:10,
+    	nav:true,
+    	autoplay:false,
+    	animateOut: 'fadeOut',
+    	animateIn: 'flipInX',
+    	responsive:{
+        0:{
+            items:1
+        }
+    	}
+	})
+
+});
