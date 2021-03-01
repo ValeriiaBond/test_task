1aef85ad45b7692a8ef168bb4062a7a8641af82b
ValeriiaBond <valeriaredkina19@gmail.com>
2021-03-01 11:32:26 +0200

    added flages to carousel

--- js/js.js
+++ js/js.js
@@ -1,48 +1,51 @@
 
 
 
-$(document).ready(function() {
-    $('.menu-burger__header').click(function() {
+$(document).ready(function () {
+    $('.menu-burger__header').click(function () {
         $('.menu-burger__header').toggleClass('open-menu');
         $('.menu__body').toggleClass('open-menu');
     });
 
     $('.benefits__carousel').owlCarousel({
-    	loop:true,
-    	margin:10,
-    	nav:true,
-    	autoplay:true,
-    	responsive:{
-        0:{
-            items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            }
         }
-    	}
-	});
+    });
 
-		$('.slider__body').owlCarousel({
-    	loop:true,
-    	margin:10,
-    	nav:true,
-    	autoplay:false,
-    	animateOut: 'fadeOut',
-    	animateIn: 'flipInX',
-    	responsive:{
-        0:{
-            items:1
+    $('.slider__body').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: false,
+        animateOut: 'fadeOut',
+        animateIn: 'flipInX',
+        responsive: {
+            0: {
+                items: 1
+            }
         }
-    	}
-	});
+    });
 
-	$('.suggestion__carousel').owlCarousel({
-		center: true,
-    items:2,
-    loop:true,
-    margin:10,
-    responsive:{
-        600:{
-            items:4
+    $('.suggestion__carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 10,
+        autoWidth: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            750: {
+                items: 5
+            }
         }
-    }
-});
+    });
 
 });
